3d6f04f01e8fefd54cfd7499dc8d1590ef924506
Zach Tumbleson <ztumbleson1@gmail.com>
2026-05-09 17:11:16 -0400

    #3 - Ingest supabase schema from remote server, set up basic supabaseClient connection

---
+++ src/backend/src/supabaseClient.js
@@ -0,0 +1,14 @@
+//
+// supabaseClient.js
+// 05-09-2026
+// Creates supabase/postgres database connection
+//
+
+const { createClient } = require('@supabase/supabase-js');
+
+const supabase = createClient(
+  process.env.SUPABASE_URL,
+  process.env.SUPABASE_SERVICE_ROLE_KEY
+);
+
+module.exports = supabase;
